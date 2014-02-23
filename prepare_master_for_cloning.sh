8f19e37cc284cc18a36272d39bb821897740e5ed
root <root@gprfs038.sbu.lab.eng.bos.redhat.com>
2014-02-23 03:30:37 -0500

    put eth1 in golden copy

--- prepare_master_for_cloning.sh
+++ prepare_master_for_cloning.sh
@@ -5,6 +5,7 @@ sshpass -p 100yard- ssh-copy-id -o StrictHostKeyChecking="no" -i ~/.ssh/id_rsa.p
 scp /root/setup/rhel7.repo $IP:/etc/yum.repos.d
 scp /root/setup/gluster.repo $IP:/etc/yum.repos.d
 scp /root/setup/onboot.sh $IP:~
+scp /root/setup/ifcfg-eth1 $IP:/etc/sysconfig/network-scripts
 scp /root/setup/onboot.service $IP:/etc/systemd/system
 ssh $IP chmod +x onboot.sh
 ssh $IP systemctl enable onboot.service
