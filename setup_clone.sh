a8bf09d3ff774f0708e397d659a9f734e49e6a27
Dan Lambright <dlambrig@redhat.com>
2014-02-24 02:35:36 -0500

    use eth1 for setup

--- setup_clone.sh
+++ setup_clone.sh
@@ -1,7 +1,7 @@
 for host in $1; do
 	for vm in `seq -s ' ' $2 $3` ; do
 		vmname="${host}_${vm}"
-		IP=`ssh $host /root/setup/get_ip.sh $vmname`
+		IP=`ssh $host /root/setup/get_ip_eth1.sh $vmname`
 		if [ -z "$IP" ]; then
 			echo No IP for $vmname
 			exit
