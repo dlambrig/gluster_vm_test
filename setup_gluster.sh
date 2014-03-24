2eb9ad2e1c69376cc9f9e3aa166f5083a96abc37
Dan Lambright <dlambrig@redhat.com>
2014-03-24 18:18:44 -0400

    fix fstab

--- setup_gluster.sh
+++ setup_gluster.sh
@@ -3,5 +3,5 @@ ssh $1 service glusterd start
 ssh $1 mkfs.xfs -f /dev/vdb
 ssh $1 mkdir -p /data/gv0/brick1
 ssh $1 mount /dev/vdb /data/gv0/brick1/
-ssh $1 "echo '/dev/vdb /data/brick1 xfs defaults 1 2' >> /etc/fstab"
+ssh $1 "echo '/dev/vdb /data/gv0/brick1 xfs defaults 1 2' >> /etc/fstab"
 
