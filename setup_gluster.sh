0ffc787b4ff21ee0c54ad808f95fb46a176e7238
Dan Lambright <dlambrig@redhat.com>
2014-03-25 09:13:17 -0400

    strick host key checking for setup gluster

--- setup_gluster.sh
+++ setup_gluster.sh
@@ -1,7 +1,7 @@
 
-ssh $1 service glusterd start
-ssh $1 mkfs.xfs -f /dev/vdb
-ssh $1 mkdir -p /data/gv0/brick1
-ssh $1 mount /dev/vdb /data/gv0/brick1/
-ssh $1 "echo '/dev/vdb /data/gv0/brick1 xfs defaults 1 2' >> /etc/fstab"
+ssh $1 -o StrictHostKeyChecking="no" service glusterd start
+ssh $1 -o StrictHostKeyChecking="no" mkfs.xfs -f /dev/vdb
+ssh $1 -o StrictHostKeyChecking="no" mkdir -p /data/gv0/brick1
+ssh $1 -o StrictHostKeyChecking="no" mount /dev/vdb /data/gv0/brick1/
+ssh $1 -o StrictHostKeyChecking="no" "echo '/dev/vdb /data/gv0/brick1 xfs defaults 1 2' >> /etc/fstab"
 
